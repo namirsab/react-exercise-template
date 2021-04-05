715aafdf0c988944452d2b58016ba8956b64ad1f
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-05 22:58:44 +0200

    Setup exercise 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [message, setMessage] = useState("Hello React");
+  // Your code below
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
 }
 
 export default App;
