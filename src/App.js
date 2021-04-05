57cf67a8870bb669a099e405b7e87f80ad73ab6d
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-04-05 23:44:04 +0200

    Setup exercise 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // Your code below
+
+  return (
+    <div className="App">
+      <p>Window Width: {windowWidth} </p>
+    </div>
+  );
 }
 
 export default App;
